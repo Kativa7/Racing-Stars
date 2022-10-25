0509157f5d15e6bb9c5fc09d0e4fce0bb35e792d
Katerina Ilieva <61324205+Kativa7@users.noreply.github.com>
2022-10-25 20:33:05 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const countdownTillBD = (dob) => {
   const today = new Date().getTime();
   const currentYear = new Date().getFullYear();
-  const prevYear = currentYear - 1;
   const upcomingBirthday = dob.slice(0, -4).concat(currentYear);
-  const lastBirthday = dob.slice(0, -4).concat(prevYear);
   const upcomingBirthdayFormat = new Date(upcomingBirthday).getTime();
-  const lastBirthdayFormat = new Date(lastBirthday).getTime();
+  const BirthdayFormat = new Date(dob).getTime();
 
   const timeLeft = upcomingBirthdayFormat - today;
-  const timePassed = today - lastBirthdayFormat;
+  const timePassed = today - BirthdayFormat;
   const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24)) + 1;
   const daysGone = Math.floor(timePassed / (1000 * 60 * 60 * 24));
   console.log(`Days left: ${daysLeft} Days passed: ${daysGone}`);
@@ -19,4 +17,4 @@ const countdownTillBD = (dob) => {
   }
 };
 
-countdownTillBD("October 25, 1989");
+countdownTillBD("October 25, 2020");
